a70b504862d3fcfb4f9a5bd58eca189667234ce3
assimdll <a.diallo_etu@enset-media.ac.ma>
2022-05-05 21:41:36 +0000

    suite..

--- app/src/main/java/com/example/testeteval/MainActivity.java
+++ app/src/main/java/com/example/testeteval/MainActivity.java
@@ -3,6 +3,8 @@ package com.example.testeteval;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.os.StrictMode;
+import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
@@ -11,42 +13,28 @@ import android.widget.EditText;
 import android.widget.Spinner;
 import android.widget.TextView;
 
+import com.example.testeteval.model.Etudiant;
+import com.example.testeteval.model.Professeur;
+import com.example.testeteval.service.RestServiceAPI;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
 public class MainActivity extends AppCompatActivity {
-    String[] etudiants = {"toto", "momo", "fifi", "lili"};
-    String[] professeurs = {"omar", "ali", "assimdll", "mohamed"};
-    String[] mdpEtd = {"azerty", "1234", "0000", "user"};
-    String[] mdpProf = {"yuiop", "12345", "abcd", "8888"};
     String[] professions = {"Etudiant", "Professeur"};
 
-    public static int findIndex(String arr[], String mot)
-    {
-        // if array is Null
-        if (arr == null) {
-            return -1;
-        }
-        // find length of array
-        int len = arr.length;
-        int i = 0;
-        // traverse in the array
-        while (i < len) {
-
-            // if the i-th element is t
-            // then return the index
-            if (arr[i] == mot) {
-                return i;
-            }
-            else {
-                i = i + 1;
-            }
-        }
-        return -1;
-    }
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        StrictMode.ThreadPolicy strictMode=new StrictMode.ThreadPolicy.Builder().permitAll().build();
+        StrictMode.setThreadPolicy(strictMode);
 
         EditText username = findViewById(R.id.username);
         EditText password = findViewById(R.id.password);
@@ -57,6 +45,10 @@ public class MainActivity extends AppCompatActivity {
         ArrayAdapter<String> dataAdapterR = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, professions);
         dataAdapterR.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         profession.setAdapter(dataAdapterR);
+
+        Retrofit retrofit=new Retrofit.Builder().baseUrl("https://localhost:8087/")
+                .addConverterFactory(GsonConverterFactory.create()).build();
+        RestServiceAPI serviceAPI=retrofit.create(RestServiceAPI.class);
 
         button.setOnClickListener(new View.OnClickListener() {
             String selected_profession ="";
@@ -75,24 +67,53 @@ public class MainActivity extends AppCompatActivity {
                     }
                 });
 
+                String key = username.getText().toString() ;
+
                 if(profession.getSelectedItem().toString()=="Etudiant"){
-                    int i = findIndex(etudiants,username.getText().toString());
-                    System.out.println(i);
-                    if(mdpEtd[i]==password.getText().toString()){
-                        badcred.setText("Vous etes connectés en tant qu'étudiant");
-                    }
-                    else{
-                        badcred.setText("Merci de verifier vos informations de onnexion");
-                    }
+                    Call<Etudiant> callEtudiant = serviceAPI.etudiantByUserName(key);
+                    callEtudiant.enqueue(new Callback<Etudiant>() {
+                        @Override
+                        public void onResponse(Call<Etudiant> call, Response<Etudiant> response) {
+                            Etudiant etud = response.body();
+                            if(etud.getPassword()==password.getText().toString()){
+                                badcred.setText("Vous etes connectés en tant qu'étudiant");
+                            }
+                            else{
+                                badcred.setText("Merci de verifier vos informations de onnexion");
+                            }
+
+                        }
+
+                        @Override
+                        public void onFailure(Call<Etudiant> call, Throwable t) {
+                            Log.e("error","Erreur de réseau");
+                        }
+                    });
                 }
+
+
                 if(profession.getSelectedItem().toString()=="Professeur"){
-                    int i = findIndex(professeurs,username.getText().toString());
-                    if(mdpProf[i]==password.getText().toString()){
-                        badcred.setText("Vous etes connectés en tant que professeur");
-                    }
-                    else{
-                        badcred.setText("Merci de verifier vos informations de onnexion");
-                    }
+                    Call<Professeur> callProf = serviceAPI.profByUserName(key);
+                    callProf.enqueue(new Callback<Professeur>() {
+                        @Override
+                        public void onResponse(Call<Professeur> call, Response<Professeur> response) {
+                            Professeur prof = response.body();
+                            if(prof.getPassword()==password.getText().toString()){
+                                badcred.setText("Vous etes connectés en tant que professeur");
+                            }
+                            else{
+                                badcred.setText("Merci de verifier vos informations de onnexion");
+                            }
+                        }
+
+                        @Override
+                        public void onFailure(Call<Professeur> call, Throwable t) {
+                            Log.e("error","Erreur de réseau");
+                        }
+                    });
+
+
+
                 }
 
                 System.out.println("Selectionnéééééééééééé "+profession.getSelectedItem().toString());
